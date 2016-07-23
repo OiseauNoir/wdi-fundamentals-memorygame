531494507110bb8e615fe26147345ea2372f8523
Jesse Houghton <MyMacbook@Jesses-MacBook-Pro.local>
2016-07-23 12:50:45 -0500

    Added JS Game Code

--- Game_Code/main.js
+++ Game_Code/main.js
@@ -2,3 +2,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	alert('Congrats! You found a match!')
+} else {
+	alert('Bummer! Try again!')
+}
